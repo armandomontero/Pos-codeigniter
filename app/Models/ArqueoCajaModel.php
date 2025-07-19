17006263acfa1eb9e4f54a468dec04549ca7ed1e
ArmandoDev <amonterof@gmail.com>
2025-07-19 15:05:40 -0400

    correccion nombre modelo

--- app/Models/ArqueoCajaModel.php
+++ app/Models/ArqueoCajaModel.php
@@ -2,7 +2,7 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-class CajasModel extends Model{
+class ArqueoCajaModel extends Model{
     protected $table      = 'arqueo_caja';
     protected $primaryKey = 'id';
 
