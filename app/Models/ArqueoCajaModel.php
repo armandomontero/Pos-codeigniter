85da835a81bf20d5df1a8998cbd7db2c586bf80e
ArmandoDev <amonterof@gmail.com>
2025-07-19 14:07:10 -0400

    model arqueo caja

---
+++ app/Models/ArqueoCajaModel.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Models;
+use CodeIgniter\Model;
+
+class CajasModel extends Model{
+    protected $table      = 'arqueo_caja';
+    protected $primaryKey = 'id';
+
+    protected $useAutoIncrement = true; 
+
+    protected $returnType     = 'array';
+    protected $useSoftDeletes = false;
+
+    protected $allowedFields = ['id_caja', 'id_usuario', 'fecha_inicio', 'fecha_fin', 
+    'monto_inicial', 'monto_final', 'total_ventas', 'status'];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
+
+?>
